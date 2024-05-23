0d878c48c09766dd0cd28205ad4ded4722f64c47
Vikram Kushwaha <vikramrobvikki@gmail.com>
2024-05-23 22:00:50 +0900

    basic network AWS

---
+++ terraform/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.50.0"
+    }
+  }
+  required_version = ">=1.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "bucket-to-store-terraform-state-pipeline-eks-app" 
+    key            = "terraform/state"
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+variable "region" {
+  description = "The AWS region where resources should be created"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "name_prefix" {
+  description = "he naming prefix to use for naming the created resources (ex: sample-app-eks-subnet1)"
+  type        = string
+}
